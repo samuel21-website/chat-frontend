ae522e0ac26b89c051f60033b649fe9c0997d71f
samuel21 <topgunmaverick0121@gmail.com>
2025-08-02 15:20:07 +0900

    � ChatRoom 연결 복구, 약관 라우팅 및 광고 코드 포함

--- src/App.js
+++ src/App.js
@@ -1,54 +1,44 @@
-// src/App.js
-import React, { useEffect, useRef, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import ChatRoom from './ChatRoom';
-import Terms from './pages/Terms';
-import Privacy from './pages/Privacy';
-import Cookie from './pages/Cookie';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ChatRoom from "./ChatRoom";
+import Terms from "./pages/Terms";
+import Privacy from "./pages/Privacy";
+import CookiePolicy from "./pages/CookiePolicy";
+import "./App.css";
 
 function App() {
-  const adRef = useRef(null);
-  const [isAdLoaded, setIsAdLoaded] = useState(false);
-
-  useEffect(() => {
-    // 광고 스크립트 추가
-    if (!isAdLoaded) {
-      const script = document.createElement('script');
-      script.async = true;
-      script.src = '//t1.daumcdn.net/kas/static/ba.min.js';
-      script.onload = () => setIsAdLoaded(true);
-      adRef.current?.appendChild(script);
-    }
-  }, [isAdLoaded]);
-
   return (
     <Router>
       <div className="App">
-        <nav>
-          <Link to="/">채팅</Link> |{" "}
-          <Link to="/terms">이용약관</Link> |{" "}
-          <Link to="/privacy">개인정보처리방침</Link> |{" "}
-          <Link to="/cookie">쿠키정책</Link>
-        </nav>
-
         <Routes>
           <Route path="/" element={<ChatRoom />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/privacy" element={<Privacy />} />
-          <Route path="/cookie" element={<Cookie />} />
+          <Route path="/cookie" element={<CookiePolicy />} />
         </Routes>
 
-        {/* 광고 배너 영역 */}
-        <div className="ad-banner" ref={adRef}>
-          <ins className="kakao_ad_area"
-            style={{ display: 'none' }}
-            data-ad-unit="DAN-6g82BnhMT7gbh8nR"  // ← 이 부분은 당신의 광고 단위 ID로 교체!
-            data-ad-width="320"
-            data-ad-height="100">
-          </ins>
-        </div>
+        <footer className="footer">
+          <Link to="/terms">이용약관</Link>
+          <Link to="/privacy">개인정보처리방침</Link>
+          <Link to="/cookie">쿠키 정책</Link>
+
+          {/* 광고 배너 */}
+          <div className="ad-banner">
+            <ins className="kakao_ad_area"
+              style={{ display: "none" }}
+              data-ad-unit="DAN-6g82BnhMT7gbh8nR"
+              data-ad-width="320"
+              data-ad-height="100"
+            ></ins>
+          </div>
+        </footer>
       </div>
+      {/* 광고 스크립트 삽입 */}
+      <script
+        type="text/javascript"
+        src="//t1.daumcdn.net/kas/static/ba.min.js"
+        async
+      ></script>
     </Router>
   );
 }
